1dcb12fa0d31709bbdd7d65d3c3909f1330c8993
Geomgee <148935802+Geomgee@users.noreply.github.com>
2024-02-13 15:51:22 +0900

    2024-02-13 | 박검지 | 마이페이지 & 피드 이미지 추가, footer design 변경, Common 색상 변경

---
+++ src/main/java/com/ex/springboot/controller/MemberController.java
@@ -0,0 +1,27 @@
+package com.ex.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ex.springboot.dto.MemberDTO;
+
+@Controller
+@RequestMapping
+public class MemberController {
+	
+	@GetMapping("/addMember")
+	public String addMember() {
+		return "thymeleaf/Member/addMember";
+	}
+	
+	@PostMapping("/addMember")
+	public String addMemberForm(MemberDTO dto) {
+		
+		
+		
+		return "redirect:/";
+	}
+
+}
