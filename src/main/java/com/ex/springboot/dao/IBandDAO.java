44fdc819b39412b57f66a46a65a60e68246af31c
GREEN <GREEN@G704-07>
2024-02-14 09:52:15 +0900

    24-02-14 | 장창엽 | 밴드 만들기 insert, myBand 페이지로 이동태그 작성

--- src/main/java/com/ex/springboot/dao/IBandDAO.java
+++ src/main/java/com/ex/springboot/dao/IBandDAO.java
@@ -3,13 +3,19 @@ package com.ex.springboot.dao;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import com.ex.springboot.dto.BandDTO;
 
 @Mapper // 인터페이스를 xml로 구현하겠다.
 public interface IBandDAO {
-	public List<BandDTO> list();
+	public List<BandDTO> bandList();
 	
-	public int bandCreateDao();
+	public BandDTO myBand(String band_code);
+	
+	public int bandCreateDao(@Param("band_name") String band_name, 
+			@Param("band_admin") String band_admin, 
+			@Param("band_content") String band_content,
+			@Param("band_thumnail") String band_thumnail);
 }
