e0fa2cc6b415673a14dd5d0fb9bc618286af282b
GREEN <GREEN@G704-07>
2024-02-16 13:11:56 +0900

    24-02-16| 장창엽 | 밴드 피드 crud 작업

--- src/main/java/com/ex/springboot/dto/BandFeedDTO.java
+++ src/main/java/com/ex/springboot/dto/BandFeedDTO.java
@@ -7,9 +7,10 @@ import lombok.Setter;
 @Setter
 public class BandFeedDTO {
 	private int band_feed_num; // 밴드 글 번호
+	private int band_code;  // 밴드 고유 번호 (fk)
 	private String Member_id; // 작성한 멤버 id 
-	private String Member_NickName; // 작성한 멤버 닉네임 
 	private String band_feed_content; // 밴드 글 내용
 	private String band_feed_uploadday; // 글 작성 시간
 	private String band_feed_updateday; // 글 수정 시간
+	private String Member_profileimage; // 멤버 프로필 사진
 }
