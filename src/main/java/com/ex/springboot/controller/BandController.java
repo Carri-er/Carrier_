cdaa2f1e5c478838d4d14e7adc74034b9cac00eb
GREEN <GREEN@G704-07>
2024-02-19 17:57:24 +0900

    24-02-19 | 장창엽 | 학원 작업 마무리

--- src/main/java/com/ex/springboot/controller/BandController.java
+++ src/main/java/com/ex/springboot/controller/BandController.java
@@ -58,17 +58,19 @@ public class BandController {
 	
 	//밴드 세부 페이지로 이동
 	@GetMapping("/myBand")
-	public String myBand(Model model, HttpServletRequest request) {
+	public String myBand(Model model, HttpServletRequest request,HttpSession session) {
 		String band_code = request.getParameter("bandUrl");
-		
 		int num_band_code = Integer.parseInt(band_code);
 		
-		HttpSession session = request.getSession();
-		String loginId = (String) session.getAttribute("Member_Id");
+		if( session.getAttribute("Member_Id") != null) {
+			String loginId = (String) session.getAttribute("Member_Id");
+			model.addAttribute("checkMember",bandDao.checkJoinMember(num_band_code, loginId));
+		}
+		
 		model.addAttribute("myBandList",bandDao.myBand(num_band_code));
 		
 		model.addAttribute("myBandFeedList",bandDao.bandFeedList(band_code));
-		model.addAttribute("checkMember",bandDao.checkJoinMember(num_band_code, loginId));
+		
 		
 		
 		
@@ -217,6 +219,10 @@ public class BandController {
 		public String addBandMember(Model model, HttpServletRequest request, BandDTO bandDto) {
 			String str_band_code = request.getParameter("bandCode");
 			String joinMemberId = request.getParameter("joinMemberId");
+			
+			if(joinMemberId.equals("") || joinMemberId.equals(null)) {
+				return "redirect:/login?msg=5";
+			}
 			
 			int num_band_code = Integer.parseInt(str_band_code);
 			
