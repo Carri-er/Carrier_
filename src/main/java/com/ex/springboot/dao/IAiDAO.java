e9c02ac0bd935b8836b9d1ea8b4547f485040506
jihoo16 <154419004+jihoo16@users.noreply.github.com>
2024-02-21 20:16:06 +0900

    24.02.21 | 이지후 | 여행 돋보기에서 map으로 출력 50% 구현 완료

--- src/main/java/com/ex/springboot/dao/IAiDAO.java
+++ src/main/java/com/ex/springboot/dao/IAiDAO.java
@@ -12,4 +12,9 @@ import com.ex.springboot.dto.EventDTO;
 @Mapper
 public interface IAiDAO {
 	public List<EventDTO> list(String area, String thema,String thema2,String thema3,String thema4,String thema5,String thema6,String thema7,String thema8,String thema9);
+	public List<EventDTO> list2(String area, String thema,String thema2,String thema3,String thema4,String thema5,String thema6,String thema7,String thema8,String thema9);
+	public List<EventDTO> listCafe(String area, String thema);
+	public List<EventDTO> listFood(String area, String thema);
+	public List<EventDTO> listAll(String area, String thema,String thema2);
+	public List<EventDTO> listAll2(String area, String thema,String thema2);
 }
