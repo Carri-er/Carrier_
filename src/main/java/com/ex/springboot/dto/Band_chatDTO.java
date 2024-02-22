a0013db25060cb1412b0a0686f108b08ea778ae7
GREEN <GREEN@G704-07>
2024-02-22 21:02:27 +0900

    24-02-22 | 장창엽 | 밴드 채팅 구현

--- src/main/java/com/ex/springboot/dto/Band_chatDTO.java
+++ src/main/java/com/ex/springboot/dto/Band_chatDTO.java
@@ -12,8 +12,9 @@ import lombok.Setter;
 public class Band_chatDTO {
 	private String band_chat_room;  // 채팅방 이름 (밴드 코드와 채팅을하는 두명의 id를 조합하여 데이터 기입)
 	private String band_code; 		// 밴드 코드 : 어느 밴드에서 채팅을 하였는지.
-	private String chat_myId;		// 채팅을한 사람의 ID : 로그인한 본인 ID
-	private String chat_partnerId;  // 채팅을 하는 상대방의 ID
+	private String chat_Id;		// 채팅을한 사람의 ID
 	private String chat_message;	// 채팅 메세지
 	private String chat_time;		// 채팅을 친 시간
+	private String Member_profileimage; // 채팅 친 사람의 프로필 이미지
+
 }
