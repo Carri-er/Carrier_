946b353e8a11ceef788345571ca948385502bd70
GREEN <GREEN@G704-07>
2024-02-27 18:51:08 +0900

    24-02-27 | 장창엽 | 지도 작업 중

--- src/main/java/com/ex/springboot/controller/CarrierController.java
+++ src/main/java/com/ex/springboot/controller/CarrierController.java
@@ -1,20 +1,23 @@
 package com.ex.springboot.controller;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ex.springboot.dao.IFeedDAO;
+
 @Controller
 @RequestMapping
 public class CarrierController {
-	@Autowired
-	private com.ex.springboot.dao.IBandDAO bandDao; // 다형성
 	
-   @GetMapping("/")	
+   @Autowired
+   private com.ex.springboot.dao.IBandDAO bandDao; // 다형성
+   @Autowired
+   IFeedDAO feed_dao;
+   
+   @GetMapping("/")   
    public String main() {
       return "thymeleaf/home/home";
    }
@@ -22,21 +25,16 @@ public class CarrierController {
 
    @GetMapping("/home")
    public String home(Model model) {
-	   
-	  model.addAttribute("randomBandList_home", bandDao.randomBandList_home());
-	   
-      return "thymeleaf/home/home2";
+      
+     model.addAttribute("randomBandList_home", bandDao.randomBandList_home());
+     model.addAttribute("feedList", feed_dao.feedList_random());
+      
+      return "thymeleaf/home/home";
    }
 
 
-   @GetMapping("/map")
-   public String map() {
-      return "thymeleaf/map/map_0221";
-   }
    
-
-
-
+   
 
 
 }
---
+++ src/main/java/com/ex/springboot/controller/MapController.java
@@ -0,0 +1,16 @@
+package com.ex.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MapController {
+	
+	
+	@GetMapping("/map")
+	   public String map() {
+		  
+		   
+	      return "thymeleaf/map/map_0221";
+	   }
+}
