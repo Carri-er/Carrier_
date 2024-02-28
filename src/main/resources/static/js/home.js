dd9c3430ad4886cc96b6d6c421be03673ba34d04
Geomgee <148935802+Geomgee@users.noreply.github.com>
2024-02-28 21:07:41 +0900

    24-02-28 | 박검지 | home & band 레이아웃 작업중

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -7,6 +7,14 @@ $(document).ready(function() {
     let dl_refresh_button = $(".dl_refresh_button span");
     let clickEl;
 
+    // section 2 nav active 추가
+    homenavlist.click(function() {
+        // 클릭된 요소에만 active 클래스 추가하고 나머지 요소에서는 제거
+        homenavlist.removeClass("active");
+        $(this).addClass("active");
+    });
+
+
     function hideAllElements() {
         dlment.hide();
         homedlimgAIcokcok.hide();
@@ -14,6 +22,7 @@ $(document).ready(function() {
         home_dl_img_AIPlaner.hide();
         dl_refresh_button.hide();
     }
+    
 
     function showElementAtIndex(index) {
         hideAllElements();
