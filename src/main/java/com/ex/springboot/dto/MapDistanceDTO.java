c93455d723196a43ac18dd2b15c63ecb7b452de8
GREEN <GREEN@G704-07>
2024-02-29 17:14:36 +0900

    24-02-29 | 장창엽 | 지도 작업 중

---
+++ src/main/java/com/ex/springboot/dto/MapDistanceDTO.java
@@ -0,0 +1,20 @@
+package com.ex.springboot.dto;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+public class MapDistanceDTO implements Comparable<MapDistanceDTO> {
+	private EventDTO event;
+	private String distance;
+	
+	 @Override
+	    public int compareTo(MapDistanceDTO o) {
+	        // 거리를 기준으로 오름차순으로 정렬합니다.
+	        double distance1 = Double.parseDouble(this.distance);
+	        double distance2 = Double.parseDouble(o.getDistance());
+	        return Double.compare(distance1, distance2);
+	    }
+}
